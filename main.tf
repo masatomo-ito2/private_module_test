6412633ca55b52c1797714b9f736cffe22ac966a
masa3 <masa@hashicorp.com>
2019-01-27 16:01:57 +0900

    added in/out

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 module "masademo" {
   source  = "ptfe.masa.hashidemos.io/org1/masademo/local"
   version = "0.1.1"
+
+	// Input parameter for module
+	count = "${var.num_of_files}"
 }
+
+output {
+	value = "${module.masademo.filenames}"
+}	
